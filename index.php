8b2db04c3809ed4dbb11441cffd209e05ce75b6f
SutkusM <47983893+SutkusM@users.noreply.github.com>
2019-10-18 17:23:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Case 3</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <br><h2 class="text-center">Your CRON JOBS tasks:</h2><br>
+            <h5><li>Open your Hostinger's account</li></h5><br>
+            <h3>User claims that his CronJobs are not working.<h3>
+            <!--<h5><li>Create a custom script and set it to be executed every minute to check if they are working</li></h5>-->
+            <h5><li>Setup a CronJob that would create a backup of a database.</li></h5>
+            <br><h3>I'm trying to setup CronJob: <br><b>wget -O /dev/null https://society.lt/cron.php 2<&1 dev/null</b></h3>
+            <h5><li>Why it's not working?</li>
+            <li>What are the alternatives?</li></h5>
+    </div>
+</body>
+</html>
